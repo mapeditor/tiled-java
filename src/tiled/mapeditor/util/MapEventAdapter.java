f1ac0c94746c83048c1e48d39b377944f2a893ca
Bjørn Lindeijer <bjorn@lindeijer.nl>
2006-02-18 10:20:26 +0000

    Got rid of Map#getNullTile(), fixed some margins in ResizeDialog after internationalization and cleaned up ResizePanel a bit.

--- src/tiled/mapeditor/util/MapEventAdapter.java
+++ src/tiled/mapeditor/util/MapEventAdapter.java
@@ -5,11 +5,11 @@
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
- * 
+ *
  *  Adam Turk <aturk@biggeruniverse.com>
  *  Bjorn Lindeijer <b.lindeijer@xs4all.nl>
  */
- 
+
 package tiled.mapeditor.util;
 
 import java.awt.*;
